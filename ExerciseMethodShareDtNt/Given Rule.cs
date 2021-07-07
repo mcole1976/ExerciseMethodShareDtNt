9c6055c06034129b5fb9ddb5296f4349691a59a1
marcusc cole <marcus_c_s_cole@hotmail.com>
2021-07-06 22:50:11 -0500

    further changes for mongodb

--- ExerciseMethodShareDtNt/Given Rule.cs
+++ ExerciseMethodShareDtNt/Given Rule.cs
@@ -9,11 +9,11 @@ namespace ExerciseMethodShareDtNt
         private string _match_Category;
         private int _rank;
         private string _exercise;
+        private int _exercise_ID;
 
         public string Match_Case { get => _match_Category; set => _match_Category = value; }
         public int Rank { get => _rank; set => _rank = value; }
         public string Exercise { get => _exercise; set => _exercise = value; }
-
-
+        public int Exercise_ID { get => _exercise_ID; set => _exercise_ID = value; }
     }
 }
