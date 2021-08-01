14eac570961e86b92601be962b261d36895a10b3
marcusc cole <marcus_c_s_cole@hotmail.com>
2021-07-31 23:55:18 -0500

    food log and ex log chanes

---
+++ ExerciseMethodShareDtNt/Food_Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseMethodShareDtNt
+{
+    public class Food_Log
+    {
+        public string Meal { get; set; }
+        public int Calorie_Count { get; set; }
+        public string Meal_Description { get; set; }
+    }
+} 
